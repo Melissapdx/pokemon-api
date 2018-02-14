1171a3d0d04a238270ffbf776c60f16301a1cb39
Melissa Orgill <orgillmelissa33@gmail.com>
2018-02-14 12:34:18 -0800

    updated get method to retrieve a single pokemon

--- api.py
+++ api.py
@@ -5,14 +5,41 @@ app = Flask(__name__)
 api = Api(app)
 api.add_resource(Pokemon, '/api/v1/pokemon')
 
-pokedex = [{'number': 14, 'name': 'Kakuna'},
-            {'number': 16, 'name': 'Pidgey'},
-            {'number': 50, 'name': 'Diglett'}]
+pokedex = [{
+    'number': 14,
+    'name': 'Kakuna',
+    'type': ['bug', 'poison'],
+    'weaknesses': ['fire', 'flying', 'physic'],
+    'evolutions': [{'number': 15, 'name': 'beedrill'}]
+    }, {
+    'number': 16,
+    'name': 'Pidgey',
+    'type': ['normal', 'flying'],
+    'weaknesses': ['electric', 'ice', 'rock'],
+    'evolutions': [{'number': 17, 'name': 'Pidgeotto'},
+                   {'number': 18, 'name': 'Pidgeot'}]
+    }, {
+    'number': 50,
+    'name': 'Dugtrio',
+    'type': ['ground'],
+    'weakness': ['grass', 'ice', 'water'],
+    'evolutions':[]
+    }]
+
 
 class Pokemon(Resource):
     def get(self):
+        """Retrieve a single Pokemon from the Pokedex. Parse arguements and give results in a dictionary"""
+        parser = reqparse.RequestParser()
+        parser.add_argument('number', required=False, type=int, location='args')
+        args = parser.parse_args(strict=True)
+        number = args.get('number')
+        if number is not None:
+            if number in [pokemon['number'] for pokemon in pokedex]:
+                return [pokemon for pokemon in pokedex if pokemon['number'] == number][0]
+            else:
+                return {}
         return pokedex
-
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -20,7 +47,7 @@ class Pokemon(Resource):
         parser.add_argument('number', type=int, required=True, location='json')
 
         args = parser.parse_args(strict=True)
-        pokemon = {'name':args['name'], 'number': args['number']}
+        pokemon = {'name': args['name'], 'number': args['number']}
 
         if pokemon in pokedex:
             return {}
